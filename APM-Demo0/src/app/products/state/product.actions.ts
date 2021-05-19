19383436da925643157c1ccc23a1add230e2ff96
Youyan WU <youyan.wu@savoye.com>
2021-05-19 12:16:34 +0200

    [WY] building action creators and using strongly typed actions

---
+++ APM-Demo0/src/app/products/state/product.actions.ts
@@ -0,0 +1,13 @@
+import { createAction, props } from "@ngrx/store";
+import { Product } from "../product";
+// Actions
+export const toggleProductCode = createAction('[Product List Page] Toggle Product Code');
+export const setCurrentProduct = createAction(
+    '[Product List Page] Set Current Product',
+    props<{ product: Product}>()
+);
+export const clreaCurrentProduuct = createAction('[Product Edit Page] Clear Current Product');
+export const initCurrentProduct = createAction('[Product List Page] Init Current Product');
+export const loadProducts = createAction('[Product List Page] Load Products');
+export const loadProductsSuccess = createAction('[Product API] Load Success');
+export const loadProductsFailure = createAction('[Product API] Load Fail');
--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,5 +1,6 @@
 import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
 import * as AppState from "src/app/state/app.state";
+import * as ProductActions from "./product.actions";
 import { Product } from "../product";
 
 export interface State extends AppState.State {
@@ -18,19 +19,47 @@ const initialState: ProductState = {
     products:[],
 }
 
+// Feature Selector
 const getProductFeatureState = createFeatureSelector<ProductState>('products');
-
+// Selectos
 export const getShowProductCode = createSelector(getProductFeatureState, state => state.showProductCode);
 export const getCurrentProduct = createSelector(getProductFeatureState, state => state.currentProduct);
 export const getProducts = createSelector(getProductFeatureState, state => state.products);
 
 export const productReducer = createReducer<ProductState>(
     initialState,
-    on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
+    on(ProductActions.toggleProductCode, (state): ProductState => {
         console.log('original state: ' + JSON.stringify(state));
         return {
             ...state,
             showProductCode: !state.showProductCode
         };
+    }),
+    on(ProductActions.setCurrentProduct, (state, action) : ProductState =>{
+        console.log('original state: ' + JSON.stringify(state));
+        return {
+            ...state,
+            currentProduct: action.product
+        }
+    }),
+    on(ProductActions.clreaCurrentProduuct, (state) : ProductState =>{
+        console.log('original state: ' + JSON.stringify(state));
+        return {
+            ...state,
+            currentProduct: null
+        }
+    }),
+    on(ProductActions.initCurrentProduct, (state, action) : ProductState =>{
+        console.log('original state: ' + JSON.stringify(state));
+        return {
+            ...state,
+            currentProduct: {
+                id: 0,
+                productName: '',
+                productCode: 'New',
+                description: '',
+                starRating: 0
+            }
+        }
     })
 );
