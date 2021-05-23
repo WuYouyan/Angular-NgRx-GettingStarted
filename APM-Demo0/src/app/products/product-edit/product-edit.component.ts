1e3086d6a3734abe9f4b67b8f9df1baaca11c0ad
Youyan WU <youyan.wu@savoye.com>
2021-05-23 22:57:34 +0200

    [WY] using Async Pipe for unsubscription

--- APM-Demo0/src/app/products/product-edit/product-edit.component.ts
+++ APM-Demo0/src/app/products/product-edit/product-edit.component.ts
@@ -9,6 +9,8 @@ import { NumberValidators } from '../../shared/number.validator';
 import { Store } from '@ngrx/store';
 import { State, getCurrentProduct } from '../state/product.reducer';
 import * as ProductActions from "../state/product.actions";
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'pm-product-edit',
@@ -19,12 +21,12 @@ export class ProductEditComponent implements OnInit {
   errorMessage = '';
   productForm: FormGroup;
 
-  product: Product | null;
-
   // Use with the generic validation message class
   displayMessage: { [key: string]: string } = {};
   private validationMessages: { [key: string]: { [key: string]: string } };
   private genericValidator: GenericValidator;
+
+  product$: Observable<Product | null>;
 
   constructor(
     private fb: FormBuilder,
@@ -62,9 +64,8 @@ export class ProductEditComponent implements OnInit {
     });
 
     // Watch for changes to the currently selected product
-    //TODO: Unsubscribe
-    this.store.select(getCurrentProduct).subscribe(
-      currentProduct => this.displayProduct(currentProduct)
+    this.product$ = this.store.select(getCurrentProduct).pipe(
+      tap(product => this.displayProduct(product))
     );
 
     // Watch for value changes for validation
@@ -80,9 +81,6 @@ export class ProductEditComponent implements OnInit {
   }
 
   displayProduct(product: Product | null): void {
-    // Set the local product property
-    this.product = product;
-
     if (product) {
       // Reset the form back to pristine
       this.productForm.reset();
--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { Product } from '../product';
-import { ProductService } from '../product.service';
 import { getCurrentProduct, getProducts, getShowProductCode, State } from '../state/product.reducer';
 import * as ProductActions from "../state/product.actions";
 import { Observable } from 'rxjs';
@@ -16,29 +15,23 @@ export class ProductListComponent implements OnInit {
   pageTitle = 'Products';
   errorMessage: string;
 
-  displayCode: boolean;
-
-  products: Product[];
-
-  // Used to highlight the selected product in the list
-  selectedProduct: Product | null;
+  displayCode$: Observable<boolean>;
+  selectedProduct$: Observable<Product>;
   products$: Observable<Product[]>;
 
   constructor(
-    private productService: ProductService,
     private store: Store<State>
   ) { }
 
   ngOnInit(): void {
-    // TODO: Unsubscribe
-    this.store.select(getCurrentProduct).subscribe(
-      currentProduct => this.selectedProduct = currentProduct
-    );
-    this.products$ = this.store.select(getProducts)
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
+
+    this.products$ = this.store.select(getProducts);
+
 
     this.store.dispatch(ProductActions.loadProducts());
-    // TODO: Unsubscribe
-    this.store.select(getShowProductCode).subscribe(showProductCode => this.displayCode = showProductCode);
+
+    this.displayCode$ = this.store.select(getShowProductCode);
   }
 
   checkChanged(): void {
