4401be6406d8cb52c93c841d44459114c0c1f610
Youyan WU <youyan.wu@savoye.com>
2021-05-24 15:11:21 +0200

    [WY] strongly typing the actions with action creators
    for update product

--- APM-Demo0/src/app/products/state/product.actions.ts
+++ APM-Demo0/src/app/products/state/product.actions.ts
@@ -12,3 +12,7 @@ export const initCurrentProduct = createAction('[Product List Page] Init Current
 export const loadProducts = createAction('[Product] Load');
 export const loadProductsSuccess = createAction('[Product] Load Success', props<{products: Product[]}>());
 export const loadProductsFailure = createAction('[Product] Load Fail', props<{error: string}>());
+
+export const updateProduct = createAction('[Product] Update Product', props<{ product: Product }>());
+export const updateProductSuccess = createAction('[Product] Update Product Success', props<{ product: Product }>());
+export const updateProductFailure = createAction('[Product] Update Product Fail', props<{error: string}>());
